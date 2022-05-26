41d9392814b24248fa30a213b649f7228c7d0917
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2022-05-25 19:21:59 -0600

    Se ordena el index.js y se agrega el link de linkedin y github

--- index.js
+++ index.js
@@ -7,19 +7,33 @@ bot.command("start", (ctx) => {
   Bienvenida(ctx);
 });
 
+bot.action("clima", (ctx) => {
+  ctx.answerCbQuery();
+
+  const menu = "Escribe el nombre la ciudad para saber el clima";
+  bot.telegram.sendMessage(ctx.chat.id, menu);
+});
+
+/* escucha el texto del usuario para mostrar el clima del ciudad */
+bot.on("text", (ctx) => {
+  const ciudad = ctx.message.text;
+  datosClima(ciudad)
+    .then((datos) => {
+      bot.telegram.sendMessage(
+        ctx.chat.id,
+        `El clima de ${ciudad} es: ${datos.main.temp} C°`
+      );
+    })
+    .catch(() => {
+      bot.telegram.sendMessage(ctx.chat.id, "Lugar no encontrado");
+    });
+});
+
 /* acciones */
 bot.action("creditos", (ctx) => {
   ctx.answerCbQuery();
   ctx.reply("Este bot fue creado por @WiliamsIxcoy");
 });
-
-bot.action("clima", (ctx) => {
-  ctx.answerCbQuery();
-
-  const menu = "Escribe el nombre la ciudad que deseas saber el clima";
-  bot.telegram.sendMessage(ctx.chat.id, menu);
-});
-
 /* escuchar */
 bot.hears("Salir", (ctx) => {
   bot.telegram.sendMessage(ctx.chat.id, "Adios", {
@@ -28,38 +42,24 @@ bot.hears("Salir", (ctx) => {
     },
   });
 });
-bot.on("text", (ctx) => {
-  const ciudad = ctx.message.text;
-  datosClima(ciudad)
-    .then((datos) => {
-      bot.telegram.sendMessage(ctx.chat.id, datos.weather[0].description);
-    })
-    .catch(() => {
-      bot.telegram.sendMessage(ctx.chat.id, "Lugar no encontrado");
-    });
-});
 /* funciones */
 async function Bienvenida(ctx) {
-  const mensaje = "Bienvenido a este bot";
+  const mensaje = `¡Bienvenido al bot del clima!
+
+Escribe el nombre la ciudad que deseas consultar`;
   bot.telegram.sendMessage(ctx.chat.id, mensaje, {
     reply_markup: {
       inline_keyboard: [
         [
           {
-            text: "¡Quiero saber el clima de una ciudad del mundo!",
-            callback_data: "clima",
+            text: "Mi Linkedin",
+            url: "https://www.linkedin.com/in/wiliams-ixcoy-656074229/",
           },
         ],
         [
           {
-            text: "Linkedin",
-            url: "https://www.google.com",
-          },
-        ],
-        [
-          {
-            text: "GitHub",
-            url: "https://www.google.com",
+            text: "Mi GitHub",
+            url: "https://github.com/wiliamsTI",
           },
         ],
         [
